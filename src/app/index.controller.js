104112e458267939b6947f8a67262e31da2b60d2
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2015-07-23 20:23:03 +0100

    fixed zafari JS bug

--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -21,7 +21,7 @@
     $scope.$on("$routeChangeSuccess", function handleRouteChangeEvent() {
       vm.menuItemSelected = _.chain(vm.menuItems)
         .find(function(menuItem){
-          return ("/#" + $location.path()).startsWith(menuItem.pageUrl);
+          return _.startsWith("/#" + $location.path(),menuItem.pageUrl);
         })
         .value();
     });
